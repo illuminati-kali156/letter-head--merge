1b864bfec4ed61211c395bc88024dcfac47597d7
CHANDAN PATIL <72489641+illuminati-kali156@users.noreply.github.com>
2026-01-16 19:15:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ st.markdown("""
 
     /* --- UPLOAD BOX VISIBILITY FIX --- */
     div[data-testid="stFileUploader"] {
-        background-color: rgba(255, 255, 255, 0.8) !important;
+        background-color: rgba(255, 255, 255, 1) !important;
         border: 2px dashed #4caf50;
         border-radius: 15px;
         padding: 15px;
